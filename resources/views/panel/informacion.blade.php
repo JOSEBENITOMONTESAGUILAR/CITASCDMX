b54277d2dfc1911a7f677460d07885df5bae1972
unknown <tidesarrollo@hotmail.com>
2020-06-13 20:42:05 -0500

    Nueva vista

--- resources/views/panel/informacion.blade.php
+++ resources/views/panel/informacion.blade.php
@@ -20,6 +20,12 @@
                             </div>
                         @endif
 
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                            </div>
+                            <br>
+
                             <div class="row">
                                 <div class="form-group col-md-6 text-center">
                                     <label for="nombre">NOMBRE</label>
@@ -48,6 +54,11 @@
                                 </div>
                                 <div class="col-md-3">
                                 </div>
+                            </div>
+
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
                             </div>
                     </div>
                 </div>
